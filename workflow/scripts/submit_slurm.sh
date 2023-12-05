17d0805448e0eb9dfd449504fe53f82fdb227514
wuhuiyun07 <wuhuiyun07@gmail.com>
2023-12-05 15:41:37 -0600

    submit spades job

--- workflow/scripts/submit_slurm.sh
+++ workflow/scripts/submit_slurm.sh
@@ -8,12 +8,12 @@
 #SBATCH --account=loni_virus2023      # your account name
 #SBATCH --partition=workq             # the partition
 
-export WORK_DIR=/project/awlab/wuhuiyun/pav_mgs_2023
+# export WORK_DIR=/project/awlab/wuhuiyun/pav_mgs_2023
 # Load any required modules for your HPC.
 
 module load snakemake
 module load python
 # Run snakemake
 # snakemake --profile config/slurm --latency-wait 90 --use-singularity --use-conda --configfile config/test.yaml
-
+cd /project/awlab/wuhuiyun/pav_mgs_2023
 snakemake -c 48 --use-conda -s workflow/rules/spades.smk
