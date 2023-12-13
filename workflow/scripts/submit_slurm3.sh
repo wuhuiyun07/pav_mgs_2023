ce12f61e80f23416230862790b21b76f329e73f5
wuhuiyun07 <wuhuiyun07@gmail.com>
2023-12-13 13:20:46 -0600

    modify OMP

--- workflow/scripts/submit_slurm3.sh
+++ workflow/scripts/submit_slurm3.sh
@@ -11,8 +11,11 @@
 # export WORK_DIR=/project/awlab/wuhuiyun/pav_mgs_2023
 # Load any required modules for your HPC.
 
+export OMP_NUM_THREADS=48
+
 module load snakemake
 module load python
+module load spades/3.13.0/gcc-9.3.0
 # Run snakemake
 # snakemake --profile config/slurm --latency-wait 90 --use-singularity --use-conda --configfile config/test.yaml
 # cd /project/awlab/wuhuiyun/pav_mgs_2023
@@ -38,4 +41,4 @@ export WORK_DIR=/project/awlab/wuhuiyun/pav_mgs_2023
 
 snakemake --use-conda  -c6 -j6 -s workflow/rules/spades2.smk
 
-# snakemake --use-singularity -c6 -j6 -s workflow/rules/spades2.smk
+snakemake --use-singularity -c6 -j6 -s workflow/rules/spades2.smk
